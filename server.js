2a72bbb668fd94699c3b59fc06d2d8acb682abf3
Rayanenassri <84158911+Rayanenassri@users.noreply.github.com>
2023-07-24 13:58:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,38 +1,37 @@
-const SampRcon = require('samp-rcon');
+const { SampIPCClient } = require('sa-mp-ipc');
 
-// SA-MP Server RCON configuration
-const rconOptions = {
-  host: 'your_samp_server_ip',
-  port: 7777, // Change this to your SA-MP server's RCON port
-  password: 'your_rcon_password', // Change this to your SA-MP server's RCON password
+// SA-MP Server IPC configuration
+const ipcOptions = {
+  ip: 'your_samp_server_ip',
+  port: 7777, // Change this to your SA-MP server's port
 };
 
-// Connect to SA-MP server via RCON
-const rcon = new SampRcon(rconOptions);
-rcon.connect();
+// Create an IPC client and connect to SA-MP server
+const ipcClient = new SampIPCClient(ipcOptions);
 
-// Function to send commands to SA-MP server through RCON
-function sendRconCommand(command) {
-  rcon.send(command)
-    .then((response) => {
-      console.log('Response:', response);
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
+ipcClient.on('connected', () => {
+  console.log('Connected to SA-MP server via IPC.');
+});
+
+ipcClient.on('disconnected', () => {
+  console.log('Disconnected from SA-MP server via IPC.');
+});
+
+// Function to make the bot say something in the chat
+function botSay(botName, message) {
+  ipcClient.send('botSay', { botName, message });
 }
 
-// Express setup (optional if you want to create an API endpoint)
+// Express setup
 const express = require('express');
 const app = express();
 const port = 3000; // Change this to your desired port number
 
-// Define your API endpoints
-app.get('/bot/say/:message', (req, res) => {
-  const { message } = req.params;
-  const command = `say ${message}`; // 'say' is the SA-MP command to send chat messages
-  sendRconCommand(command);
-  res.send(`Bot said: ${message}`);
+// Define your API endpoint to make the bot say something
+app.get('/bot/say/:botName/:message', (req, res) => {
+  const { botName, message } = req.params;
+  botSay(botName, message);
+  res.send(`Bot "${botName}" said: ${message}`);
 });
 
 // Start the Express server
